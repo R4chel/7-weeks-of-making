ea0fa97c6854119589e99e561bfff706307a6f91
R4chel <rae84@cornell.edu>
2023-07-07 20:59:40 -0400

    day3 ready to print

--- day3/sketch_day3.py
+++ day3/sketch_day3.py
@@ -9,11 +9,13 @@ class Day3Sketch(vsketch.SketchClass):
     width = vsketch.Param(5., decimals=2, unit="in")
     height = vsketch.Param(3., decimals=2, unit="in")
     margin = vsketch.Param(0.1, decimals=3, unit="in")
-    landscape = vsketch.Param(False)
+    landscape = vsketch.Param(True)
     pen_width = vsketch.Param(0.7, decimals=3, min_value=1e-10, unit="mm")
     num_layers = vsketch.Param(1)
     radius = vsketch.Param(0.25, decimals=3, unit="in")
     fill_probability = vsketch.Param(0.2)
+    random_scalar = vsketch.Param(1.0, decimals=3)
+    triangle_randomness = vsketch.Param(5)
 
     def random_point(self, vsk: vsketch.Vsketch):
         return Point(vsk.random(0, self.width), vsk.random(0, self.height))
@@ -21,15 +23,15 @@ class Day3Sketch(vsketch.SketchClass):
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size(f"{self.height}x{self.width}",
                  landscape=self.landscape,
-                 center=False)
+                 center=True)
         self.width = self.width - 2 * self.margin
         self.height = self.height - 2 * self.margin
         vsk.translate(self.margin, self.margin)
         vsk.penWidth(f"{self.pen_width}")
         layers = [1 + i for i in range(self.num_layers)]
 
-        for x in np.arange(0, self.width, self.radius):
-            for y in np.arange(0, self.height, self.radius):
+        for x in np.arange(0, self.width - self.radius + 1, self.radius):
+            for y in np.arange(0, self.height - self.radius + 1, self.radius):
 
                 layer = layers[int(vsk.random(0, len(layers)))]
                 vsk.stroke(layer)
@@ -39,9 +41,17 @@ class Day3Sketch(vsketch.SketchClass):
                 else:
                     vsk.noFill()
                 vsk.pushMatrix()
-                vsk.rotate(angle=vsk.random(-1, 1) * vsk.random(x + y),
+                vsk.translate(x, y)
+                vsk.rotate(angle=vsk.random(-1, 1) *
+                           vsk.random(self.random_scalar * (x + y)),
                            degrees=True)
-                vsk.square(x, y, self.radius)
+
+                vsk.triangle(
+                    vsk.random(-self.triangle_randomness,
+                               self.triangle_randomness),
+                    vsk.random(-self.triangle_randomness,
+                               self.triangle_randomness), 0, self.radius,
+                    self.radius, 0)
                 vsk.popMatrix()
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
